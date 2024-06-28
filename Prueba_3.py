3324990ce49a8b343b250862dacc57c92ecc3e9a
Ivan Figueroa Diaz <iv.figueroad@duocuc.cl>
2024-06-28 17:52:11 -0400

    Aqui inicio designando las bariables y juntandolas en una dupla

--- Prueba_3.py
+++ Prueba_3.py
@@ -3,3 +3,11 @@
 #Aqui inicio importando lo que usare a lo largo de mi programacion
 import random
 from statistics import geometric_mean
+
+#Aqui inicio designando las bariables y juntandolas en una dupla
+
+nombres = ["juan", "pedro", "maria", "marta", "ivan", 
+           "anastacio", "juana", "pepe", "david", "manuel"]
+saldos = [random.randint(500000, 1000000) for _ in range(10)]
+
+clientes = list(zip(nombres, saldos))
