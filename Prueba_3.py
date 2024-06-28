96a8f86a86e32a3cd667c12239a57e2a9bf69420
Ivan Figueroa Diaz <iv.figueroad@duocuc.cl>
2024-06-28 17:55:46 -0400

    Aqui ire definiendo los diferentes saldos y sus promedios

--- Prueba_3.py
+++ Prueba_3.py
@@ -11,3 +11,32 @@ nombres = ["juan", "pedro", "maria", "marta", "ivan",
 saldos = [random.randint(500000, 1000000) for _ in range(10)]
 
 clientes = list(zip(nombres, saldos))
+
+#Aqui ire definiendo los diferentes saldos y sus promedios
+def saldo_mas_alto(clientes):
+    max_saldo = clientes[0]
+    for cliente in clientes:
+        if cliente[1] > max_saldo[1]:
+            max_saldo = cliente
+    return max_saldo
+
+
+def saldo_mas_bajo(clientes):
+    min_saldo = clientes[0]
+    for cliente in clientes:
+        if cliente[1] < min_saldo[1]:
+            min_saldo = cliente
+    return min_saldo
+
+
+def saldo_promedio(clientes):
+    total_saldo = sum(cliente[1] for cliente in clientes)
+    return total_saldo / len(clientes)
+
+def saldo_media_geometrica(clientes):
+    saldo = [cliente[1]for cliente in clientes]
+    return geometric_mean(saldos)
+
+saldos_bajos = [cliente for cliente in clientes if cliente[1] < 666667]
+saldos_medios = [cliente for cliente in clientes if 666667 <= cliente[1] < 833334]
+saldos_superiores = [cliente for cliente in clientes if cliente[1] >= 833334]
